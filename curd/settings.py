93a7bd6e213ae617fcb1d545894dcef4d4b7f634
lokendra-singh-Rathore <80844824+lokendra-singh-Rathore@users.noreply.github.com>
2021-11-25 13:16:25 +0530

    Update settings.py

--- curd/settings.py
+++ curd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9_in22eb67r)4yy*unic#_q^d@jxmbhrk(0u1e8i-=hap=-c9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1','curdlokendra.heroku.com']
 
 
