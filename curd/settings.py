406823a43512b914713870e0342d2edd5846cd5d
lokendra-singh-Rathore <80844824+lokendra-singh-Rathore@users.noreply.github.com>
2021-11-25 13:18:41 +0530

    Update settings.py

--- curd/settings.py
+++ curd/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '9_in22eb67r)4yy*unic#_q^d@jxmbhrk(0u1e8i-=hap=-c9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','curdlokendra.heroku.com']
+ALLOWED_HOSTS = ['curdlokendra.herokuapp.com']
 
 
 # Application definition
